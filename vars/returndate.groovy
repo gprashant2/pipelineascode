7bd5fe2aff3069eee2ff66d4a60f32c32092d936
gprashant2 <gautamprashant020@gmail.com>
2019-04-18 21:53:30 +0530

    Update returndate.groovy

--- vars/returndate.groovy
+++ vars/returndate.groovy
@@ -1,6 +1,6 @@
 def printWedonlydate() {
-DATE=`date +%d-%m-%Y`
-NEW_DATE=$(date -d "+7 days" '+%m-%d-%Y')
-DAYOFWEEK=$(date +"%u")
+sh "DATE=`date +%d-%m-%Y`"
+sh "NEW_DATE=$(date -d "+7 days" '+%m-%d-%Y')"
+sh "DAYOFWEEK=$(date +"%u")"
 sh "if [ "$DAYOFWEEK" == 3 ];  then    echo $DATE; else    echo $NEW_DATE; fi"
 }
