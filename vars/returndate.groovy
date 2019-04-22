0109442e11ea9488abb5877a1a1c2c243faf0f5c
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 20:55:05 +0530

    Update returndate.groovy

--- vars/returndate.groovy
+++ vars/returndate.groovy
@@ -3,7 +3,7 @@ def date = sh returnStdout: true, script: 'date +%d-%m-%Y'
 Calendar cal = Calendar.getInstance();
 int day = cal.get(Calendar.DAY_OF_WEEK);
 
-    if (day == 1){
+    if (day == 2){
         RELEASE_BRANCH1 = "release-" + date
     }else{
         RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
