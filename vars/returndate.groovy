ba63361f54162872fced53728f3cfefa2eab411e
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 20:52:02 +0530

    Update returndate.groovy

--- vars/returndate.groovy
+++ vars/returndate.groovy
@@ -9,24 +9,5 @@ def  RELEASE_BRANCH3 = sh returnStdout: true, script: 'date -d "next wednesday" 
         RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
         RELEASE_BRANCH1 = "release-" + RELEASE_BRANCH1
         print RELEASE_BRANCH1
-    
-
-}
-
-
-
-
- //if ( dayweek.equals('Thu' )) {
-  //print date
- //}
-//def output = sh returnStdout: true, script: 'if [ "dayofweek" == 3 ];  then    echo date; else echo newdate; fi' 
- // sh "if [ "dayofweek" == 4 ];  then    echo date;  fi"
-//def output = sh returnStdout: true, script: 'if [ "$dayofweek" == 3 ];  then    echo $date; else    echo $new_date; fi'
-//def date = new Date().format("MM-dd-YYYY")
-//print date 
-//print newdate  
-//print dayofweek
-//NEW_DATE=$(date -d "+7 days" '+%m-%d-%Y')
-//DAYOFWEEK=$(date +"%u")
-//sh "if [ "$DAYOFWEEK" == 3 ];  then    echo $DATE; else    echo $NEW_DATE; fi"
-}
+    }
+                        }
