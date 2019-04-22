966506e12d8075a63ed5768a7547f7d6b07c87fe
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 10:26:15 +0530

    Update returndate.groovy

--- vars/returndate.groovy
+++ vars/returndate.groovy
@@ -8,7 +8,7 @@ if (day == 4){
 }else{
     def  nextwed = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
   //  print nextwed
-   // println RELEASE_BRANCH
+    println RELEASE_BRANCH
    // println("release-" + RELEASE_BRANCH);
 }
 
