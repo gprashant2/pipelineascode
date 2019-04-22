db4132799cec107fb92afd7ae3b7f6dfc866682d
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 13:45:28 +0530

    Update newvariable.groovy

--- vars/newvariable.groovy
+++ vars/newvariable.groovy
@@ -1,12 +1,9 @@
 
-import hudson.EnvVars
-import hudson.model.Environment
-
-def build = Thread.currentThread().executable
-def  RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
-  def vars = [RELEASE_BRANCH2: '${RELEASE_BRANCH1}']
-
-build.environments.add(0, Environment.create(new EnvVars(vars)))
+def releasebranch() {
+  //  echo "INFO: ${message}"
+   RELEASE_BRANCH2 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+        
+}
 
 
 
