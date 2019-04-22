ef28fc672eb09a6bff25cdc0d6ccd913f19b83e6
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 20:50:53 +0530

    Update returndate.groovy

--- vars/returndate.groovy
+++ vars/returndate.groovy
@@ -1,15 +1,14 @@
 def printWedonlydate() {
-//sh "DATE=`date +%d-%m-%Y`"
 def date = sh returnStdout: true, script: 'date +%d-%m-%Y'
 Calendar cal = Calendar.getInstance();
 int day = cal.get(Calendar.DAY_OF_WEEK);
-    def  RELEASE_BRANCH3 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
-if (day == 4){
-    print date
-}else{
-      RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
-    def concatt = "release-" + RELEASE_BRANCH1
-    print concatt
+def  RELEASE_BRANCH3 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+    if (day == 4){
+        print date
+    }else{
+        RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+        RELEASE_BRANCH1 = "release-" + RELEASE_BRANCH1
+        print RELEASE_BRANCH1
     
 
 }
