6441bdd3fe68331e91ce327cad66c7dbb778fe97
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 15:29:47 +0530

    Update newvariable.groovy

--- vars/newvariable.groovy
+++ vars/newvariable.groovy
@@ -2,6 +2,7 @@
 def releasebranch() {
   //  echo "INFO: ${message}"
    RELEASE_BRANCH2 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+  return RELEASE_BRANCH2;
         
 }
 
