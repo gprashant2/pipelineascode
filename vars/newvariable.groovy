144f8cbe7f9f867c3273e1b698c5e4046562739c
gprashant2 <gautamprashant020@gmail.com>
2019-04-22 11:50:46 +0530

    Update newvariable.groovy

--- vars/newvariable.groovy
+++ vars/newvariable.groovy
@@ -1,7 +1,20 @@
+def printWedonlydate() {
 import hudson.EnvVars
 import hudson.model.Environment
 
 def build = Thread.currentThread().executable
-def vars = [ENV_VAR1: 'value1', ENV_VAR2: 'value2']
+def  RELEASE_BRANCH1 = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+  def vars = [ENV_VAR1: '${RELEASE_BRANCH2}']
 
 build.environments.add(0, Environment.create(new EnvVars(vars)))
+def date = sh returnStdout: true, script: 'date +%d-%m-%Y'
+Calendar cal = Calendar.getInstance();
+int day = cal.get(Calendar.DAY_OF_WEEK);
+//if (day == 4){
+  //  print date
+//}else{
+  //  def  nextwed = sh returnStdout: true, script: 'date -d "next wednesday" +%d-%m-%Y'
+
+//}
+
+}
